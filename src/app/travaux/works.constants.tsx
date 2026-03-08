573f180fd0f26ba43b543cf881fa860cf1a3f7e8
Michaël Bourjac <michael.bourjac@gmail.com>
2026-03-08 10:34:10 +0100

    feat(works): add seminars data

---
+++ src/app/travaux/works.constants.tsx
@@ -0,0 +1,232 @@
+export const SEMINARS = {
+  semester7: {
+    label: 'Semestre 7',
+    title: 'Sciences humaines et sociales du et pour le projet',
+    description: null,
+    pictures: [],
+    list: [
+      {
+        year: '2025-2026',
+        mentoring: [
+          'Federica Gatta',
+          'Cécile Léonardi',
+          'Théa Manola',
+          'Simeon Naydenov',
+        ],
+        guests: [],
+        pictures: [],
+      },
+      {
+        year: '2024-2025',
+        mentoring: [
+          'Federica Gatta',
+          'Cécile Léonardi',
+          'Théa Manola',
+          'Simeon Naydenov',
+        ],
+        guests: [],
+        pictures: [],
+      },
+      {
+        year: '2023-2024',
+        mentoring: ['Federica Gatta', 'Cécile Léonardi', 'Théa Manola'],
+        guests: [],
+        pictures: [],
+      },
+      {
+        year: '2022-2023',
+        mentoring: [
+          'Federica Gatta',
+          'Cécile Léonardi',
+          'Théa Manola',
+          'Nicolas Tixier',
+        ],
+        guests: [
+          'Giulia Buffoli',
+          'Alain Guez',
+          'Romain Lajarge',
+          'Raphaël Zephir',
+        ],
+        pictures: [],
+      },
+      {
+        year: '2021-2022',
+        mentoring: [
+          'Marine Bourgeois',
+          'Federica Gatta',
+          'Cécile Léonardi',
+          'Théa Manola',
+          'Nicolas Tixier',
+        ],
+        guests: [
+          'Mathilde Girault',
+          'Charles Rives',
+          'Philippe Teillet',
+          'Simon Teyssou',
+        ],
+        pictures: [],
+      },
+    ],
+  },
+  semester8: {
+    label: 'Semestre 8',
+    title: 'Les controverses de la ville néolibérale',
+    description: (
+      <>
+        Sur la base de lectures et d’interventions, ce séminaire se donne pour
+        objectifs de saisir les controverses de la ville néolibérale en revenant
+        sur les concepts et théories qui lui sont associés, sur les enjeux de
+        justice sociale et spatiale qu’elles soulèvent, ainsi que sur les
+        luttes, résistances et éventuelles alternatives au capitalisme urbain.
+        Plus précisément, il s’agit de&nbsp;: comprendre les enjeux de la
+        mondialisation néolibérale et leurs effets sur les villes&nbsp;;
+        appréhender les outils de la critique de la mondialisation
+        néolibérale&nbsp;; analyser l’emboîtement des échelles dans la
+        production de la ville (matérielles, symboliques, politiques)&nbsp;;
+        savoir évaluer le sens des «&nbsp;alternatives&nbsp;» possibles.
+      </>
+    ),
+    pictures: [],
+    list: [
+      {
+        year: '2025-2026',
+        mentoring: ['Marine Bourgeois', 'Grégory Busquet'],
+        guests: ['Clément Barbier et Stéphane Tonnelat'],
+        pictures: [],
+      },
+      {
+        year: '2024-2025',
+        mentoring: ['Grégory Busquet', 'Marion Lang'],
+        guests: ['Victor Collet', 'Thibault Jouannic'],
+        pictures: [],
+      },
+      {
+        year: '2023-2024',
+        mentoring: ['Marine Bourgeois', 'Marc Higgin', 'Théa Manola'],
+        guests: ['Johanna Berthoux', 'Grégory Busquet'],
+        pictures: [],
+      },
+      {
+        year: '2022-2023',
+        mentoring: ['Marine Bourgeois', 'Federica Gatta', 'Théa Manola'],
+        guests: ['Matthieu Adam et Emeline Comby', 'Max Rousseau'],
+        pictures: [],
+      },
+      {
+        year: '2021-2022',
+        mentoring: ['Marine Bourgeois', 'Federica Gatta', 'Théa Manola'],
+        guests: ['Gilles Pinson', 'Guillaume Faburel', 'Véronique Biau'],
+        pictures: [],
+      },
+    ],
+  },
+  semester9: {
+    label: 'Semestre 9',
+    title: 'Métiers de la production des territoires habités',
+    description: (
+      <>
+        L’objectif de ce séminaire est d’aborder les métiers de la production
+        des territoires habités et leurs évolutions. Il s’agit à la fois
+        de&nbsp;: prendre connaissance des travaux théoriques en lien avec ces
+        questions (sociologie des professions)&nbsp;; prendre connaissance des
+        enjeux actuels dans les pratiques professionnelles des métiers des
+        territoires habités &nbsp;; construire des bases en termes d’outils
+        conceptuels pour penser et accompagner ces évolutions de manière
+        critique&nbsp;; commencer à construire une projection personnelle.
+      </>
+    ),
+    pictures: [],
+    list: [
+      {
+        year: '2025-2026',
+        mentoring: [
+          'Théa Manola (resp.)',
+          'Rémi Baudouï',
+          'Julia Grosinger',
+          'Elsa Guillalot',
+          'Nicolas Tixier',
+        ],
+        guests: [
+          'Adrien Barbier (Interland)',
+          'Nathan Delmas et Simeon Naydenov (Collectif Otopo)',
+          'Juliette Garcia (Atelier Nous)',
+          'Romain Gardin (Grenoble Alpes Métropole)',
+          'Clair Gauffenic (Nova7)',
+          'Élise Lemercier (Ateliers Déconcertants)',
+          'Pierre Merle (CAUE Isère)',
+          'Martin Rollin (Commune)',
+        ],
+        pictures: [],
+      },
+      {
+        year: '2024-2025',
+        mentoring: [
+          'Théa Manola (resp.)',
+          'Rémi Baudouï',
+          'Julia Grosinger',
+          'Elsa Guillalot',
+          'Nicolas Tixier',
+        ],
+        guests: [
+          'Maxence Bohn (Collectif Etc)',
+          'Bastien Dalmasso (Grenoble Alpes Métropole)',
+          'Laure Favier (Grenoble Alpes Métropole)',
+          'Roberta Ghelli (Maison de l’Architecture de l’Isère)',
+          'Élise Lemercier (Ateliers Déconcertants)',
+          'Pierre Merle (CAUE Isère)',
+          'Gabriel Sibille (Est Métropole Habitat)',
+          'Marie Wozniak (AUE)',
+        ],
+        pictures: [],
+      },
+      {
+        year: '2023-2024',
+        mentoring: [
+          'Théa Manola (resp.)',
+          'Charles Ambrosino',
+          'Julia Grosinger',
+          'Elsa Guillalot',
+          'Mathias Rollot ',
+        ],
+        guests: [
+          'Bastien Dalmasso (Métropole Grenoble Alpes)',
+          'Roberta Ghelli (Maison de l’architecture de l’Isère)',
+          'Sylvie Laroche (AURG)',
+          'Élise Lemercier (Ateliers Déconcertants)',
+          'Magali Marlin (Nantes Métropole)',
+          'Pierre Merle (CAUE Isère)',
+          'Manon Sajaloli (AURG)',
+          'Gabriel Sibille (Est Métropole Habitat)',
+          'Sylvaine Viot (UrbA 2P)',
+          'Théo Mouzard (Collectif Etc)',
+        ],
+        pictures: [],
+      },
+      {
+        year: '2022-2023',
+        mentoring: ['Théa Manola (resp.)'],
+        guests: ['Matthieu Adam et Emeline Comby', 'Max Rousseau'],
+        pictures: [],
+      },
+      {
+        year: '2021-2022',
+        mentoring: ['Marine Bourgeois', 'Federica Gatta', 'Théa Manola'],
+        guests: [
+          'Alexia Beaujeux (La traverse)',
+          'Noémie Benezeth (AURG)',
+          'Sacha Czertok (Acadie)',
+          'Manuel Domergue (Fondation Abbé Pierre)',
+          'Bastien Dalmasso (Métropole Grenoble Alpes)',
+          'Élise Lemercier (Ateliers Déconcertants)',
+          'Pierre Merle (Chargé de mission en urbanisme CAUE Isère)',
+          'Yvan Okotnikoff (ENSAG & Atelier Georges)',
+          'Éric Ruiz (Métropole Grenoble Alpes)',
+          'Gabriel Sibille (ENSAG & Est Métropole Habitat)',
+          'Irène Vladimirov (Patriarche)',
+          'Marie Wozniak (ENSAG & AUE)',
+        ],
+        pictures: [],
+      },
+    ],
+  },
+};
